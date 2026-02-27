93d129725a5305e6fa91580614a1555371c4f928
zml1206 <zhuml1206@gmail.com>
2026-02-27 15:57:03 +0800

    bump velox version

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -17,7 +17,7 @@
 set -exu
 
 VELOX_REPO=https://github.com/zml1206/velox.git
-VELOX_BRANCH=2025_08_11
+VELOX_BRANCH=2025_08_11_hf1
 VELOX_HOME=""
 RUN_SETUP_SCRIPT=ON
 VELOX_ENHANCED_REPO=https://github.com/IBM/velox.git
